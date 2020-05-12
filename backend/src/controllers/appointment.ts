dcabac3e496bcb9b7ea58108b9192232996cf56b
jcvp92 <caliche_92@hotmail.com>
2020-05-12 13:53:51 -0500

    citas_medicas

--- backend/src/controllers/appointment.ts
+++ backend/src/controllers/appointment.ts
@@ -13,7 +13,7 @@ export const createAppointment=(req:Request,res:Response)=>{
          users:body.users
    
     
-   });
+   }); 
    
    objAppointment.save((err,appointmentDB)=>{
       if (err) {
@@ -31,4 +31,76 @@ export const createAppointment=(req:Request,res:Response)=>{
   })
   
 
+}
+
+export const getAppointments = (req: Request, res: Response) => {
+    Appointment.find()
+                .exec((err, appointmenst) => {
+                    if (err) {
+                        res.status(500).json({
+                            ok:false,
+                            message:"internat_server_error", 
+                            err 
+                        })   
+                    }
+                    res.status(200).json({
+                        ok: true,
+                        appointmenst
+                    })
+                })
+}
+
+export const uptadeAppointmentById = (req: Request, res: Response) => {
+    let id = req.params.id;
+    let body = req.body
+    Appointment.findByIdAndUpdate(id, body, {new: true}, (err, appointmentDB) => {
+        if (err) {
+            res.status(500).json({
+                ok:false,
+                message:"internat_server_error", 
+                err 
+            })   
+        }
+        if (!appointmentDB) {
+            res.status(404).json({
+                ok:false,
+                err: {
+                    message: 'not found appointment'
+                }
+            })   
+        }
+
+        res.status(200).json({
+            ok: true,
+            appointment: appointmentDB
+        })
+        
+    })
+}
+
+export const deleteAppointmentById = (req: Request, res: Response) => {
+    let id = req.params.id;
+
+    Appointment.findByIdAndRemove( id , (err, appointmentDB) => {
+        if (err) {
+            res.status(500).json({
+                ok:false,
+                message:"internat_server_error", 
+                err 
+            })   
+        }
+        if (!appointmentDB) {
+            res.status(404).json({
+                ok:false,
+                err: {
+                    message: 'not found appointment'
+                }
+            })   
+        }
+        res.status(200).json({
+            ok: true,
+            message: 'appointment remove successfully',
+            appointment: appointmentDB
+        })
+    })
 }
--- backend/src/routers/appointments.ts
+++ backend/src/routers/appointments.ts
@@ -1,6 +1,9 @@
 import { Router } from "express"
-import { createAppointment } from "../controllers/appointment";
+import { createAppointment, getAppointments, uptadeAppointmentById, deleteAppointmentById } from '../controllers/appointment';
 
 
 export const appointments_router=Router();
 appointments_router.post("/appointment",createAppointment);
+appointments_router.get("/appointments", getAppointments);
+appointments_router.put("/appointment/:id", uptadeAppointmentById);
+appointments_router.delete("/appointment/:id", deleteAppointmentById);
